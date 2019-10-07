aa0d9d7a4c2a7d6bdbf06921939d0c507e6fa1a1
JoAn <joannacalinska@gmail.com>
2019-10-07 21:28:07 +0200

    refactor - adding class Model & interfaces

---
+++ src/models/Model.ts
@@ -0,0 +1,59 @@
+import { AxiosPromise, AxiosResponse } from 'axios';
+
+interface Attributes<T> {
+  get<K extends keyof T>(key: K): T[K];
+  set(value: T): void;
+  getAll(): T;
+}
+
+interface Events {
+  on(eventName: string, callback: () => void): void;
+  trigger(eventName: string): void;
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+
+interface hasId {
+  id?: number;
+}
+
+export class Model<T extends hasId> {
+  constructor(
+    private attributes: Attributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) {}
+
+  on = this.events.on;
+
+  trigger = this.events.trigger;
+
+  get = this.attributes.get;
+
+  set(update: T): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (!id) {
+      throw new Error("Can't fetch user without id");
+    }
+
+    this.sync.fetch(id).then((res: AxiosResponse): void => {
+      this.set(res.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((res: AxiosResponse): void => this.trigger('save'))
+      .catch(() => this.trigger('error'));
+  }
+}
