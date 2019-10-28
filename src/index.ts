e95836f9acc667e808ca0df3852b90153f2021ce
JoAn <joannacalinska@gmail.com>
2019-10-28 21:06:25 +0100

    render all users

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+// import { UserEdit } from './views/UserEdit';
+// import { User } from './models/User';
 
-const user = User.buildUser({ name: 'piotr', age: 10 });
+// const user = User.buildUser({ name: 'piotr', age: 10 });
 
-const root = document.getElementById('root');
+// const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
 
-  userEdit.render();
-} else {
-  throw new Error('No root element');
-}
+//   userEdit.render();
+// } else {
+//   throw new Error('No root element');
+// }
+
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+
+const url = 'http://localhost:3000/users';
+const users = new Collection(url, (json: UserProps) => User.buildUser(json));
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
